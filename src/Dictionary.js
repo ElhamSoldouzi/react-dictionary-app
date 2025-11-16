a160f9e851230bd6fa58002007781beb8442f158
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2025-11-16 23:47:34 +0330

    fixing some issue

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css";
 import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -14,6 +14,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -21,15 +22,11 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input
-          type="search"
-          onChange={handleKeywordChange}
-          placeholder="Enter a word"
-          autoFocus={true}
-        />
+        <input type="search" onChange={handleKeywordChange} autoFocus={true} />
       </form>
       <Results results={results} />
     </div>
