abeda0576558ec361e083214876cd2e2d45349fa
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2025-11-17 11:54:56 +0330

    adding synonyms to project

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,34 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
+
+  // Limit definitions to 4
+  const limitedDefinitions = props.meaning.definitions.slice(0, 4);
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      {limitedDefinitions.map(function (definition, index) {
         return (
-          <div key={index}>
+          <div key={index} className="definition-card">
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
-              <br />
-              <em>{definition.example}</em>
             </p>
+            {definition.example && (
+              <p>
+                <strong>Example:</strong>
+                <em>{definition.example}</em>
+              </p>
+            )}
+            {definition.synonyms && definition.synonyms.length > 0 && (
+              <p>
+                <strong>Synonyms:</strong>
+                <Synonyms synonyms={definition.synonyms} />
+              </p>
+            )}
           </div>
         );
       })}
